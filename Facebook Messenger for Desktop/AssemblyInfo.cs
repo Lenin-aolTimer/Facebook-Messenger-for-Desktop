cf55fbdbecd56d7f8e9fd1111040d237d1f19899
alexrasen <admin@onecloud.cl>
2016-04-12 02:58:50 -0300

    Added files via upload

---
+++ Facebook Messenger for Desktop/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿// Assembly Facebook Messenger for Desktop, Version 1.0.0.0
+
+[assembly: System.Reflection.AssemblyVersion("1.0.0.0")]
+[assembly: System.Reflection.AssemblyDescription("Aplicaci\x00f3n simple y liviana para utilizar el chat de Facebook. Requiere conexi\x00f3n a Internet.")]
+[assembly: System.Reflection.AssemblyConfiguration("")]
+[assembly: System.Reflection.AssemblyCompany("oneCloud.cl")]
+[assembly: System.Runtime.CompilerServices.RuntimeCompatibility(WrapNonExceptionThrows=true)]
+[assembly: System.Reflection.AssemblyTrademark("oneCloud.cl")]
+[assembly: System.Runtime.CompilerServices.CompilationRelaxations(8)]
+[assembly: System.Reflection.AssemblyTitle("Facebook Messenger for Desktop")]
+[assembly: System.Reflection.AssemblyProduct("Facebook Messenger for Desktop")]
+[assembly: System.Reflection.AssemblyCopyright("Copyright \x00a9 AlexRasen 2016")]
+[assembly: System.Runtime.Versioning.TargetFramework(".NETFramework,Version=v4.5", FrameworkDisplayName=".NET Framework 4.5")]
+[assembly: System.Runtime.InteropServices.ComVisible(false)]
+[assembly: System.Runtime.InteropServices.Guid("84e51ba4-be79-4380-9264-d6f6638cb644")]
+[assembly: System.Reflection.AssemblyFileVersion("2.0.0.0")]
+[assembly: System.Diagnostics.Debuggable(System.Diagnostics.DebuggableAttribute.DebuggingModes.DisableOptimizations | System.Diagnostics.DebuggableAttribute.DebuggingModes.EnableEditAndContinue | System.Diagnostics.DebuggableAttribute.DebuggingModes.IgnoreSymbolStoreSequencePoints | System.Diagnostics.DebuggableAttribute.DebuggingModes.Default)]
+
---
+++ Facebook Messenger for Desktop/Facebook_Messenger_for_Desktop/frMain.cs
@@ -0,0 +1,74 @@
+﻿namespace Facebook_Messenger_for_Desktop
+{
+    using System;
+    using System.ComponentModel;
+    using System.Diagnostics;
+    using System.Drawing;
+    using System.Windows.Forms;
+
+    public class frMain : Form
+    {
+        private IContainer components = null;
+        private Process currentProcess = Process.GetCurrentProcess();
+        private Process[] processes = Process.GetProcesses();
+        private WebBrowser webBrowser1;
+
+        public frMain()
+        {
+            if (this.isRunning())
+            {
+                MessageBox.Show("La aplicaci\x00f3n ya se encuentra en ejecuci\x00f3n", "Error", MessageBoxButtons.OK, MessageBoxIcon.Hand);
+                base.Close();
+                Application.Exit();
+            }
+            else
+            {
+                this.InitializeComponent();
+            }
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (this.components != null))
+            {
+                this.components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            ComponentResourceManager manager = new ComponentResourceManager(typeof(frMain));
+            this.webBrowser1 = new WebBrowser();
+            base.SuspendLayout();
+            this.webBrowser1.Dock = DockStyle.Fill;
+            this.webBrowser1.Location = new Point(0, 0);
+            this.webBrowser1.MinimumSize = new Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new Size(0x3bf, 0x386);
+            this.webBrowser1.TabIndex = 0;
+            this.webBrowser1.Url = new Uri("https://www.messenger.com", UriKind.Absolute);
+            base.AutoScaleDimensions = new SizeF(8f, 16f);
+            base.AutoScaleMode = AutoScaleMode.Font;
+            base.ClientSize = new Size(0x3bf, 0x386);
+            base.Controls.Add(this.webBrowser1);
+            base.Icon = (Icon) manager.GetObject("$this.Icon");
+            base.Name = "frMain";
+            this.Text = "Facebook Messenger";
+            base.ResumeLayout(false);
+        }
+
+        private bool isRunning()
+        {
+            foreach (Process process in this.processes)
+            {
+                if ((process.Id != this.currentProcess.Id) && (process.ProcessName == this.currentProcess.ProcessName))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
+
